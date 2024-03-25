5792e37a886f4117b8285aa611150102f92c7837
Sushentsova <natalya.rozikulyeva@ya.ru>
2024-03-26 00:00:30 +0300

    системные изменения в task00.sourcelink.json

--- task00/obj/Debug/net8.0/task00.AssemblyInfo.cs
+++ task00/obj/Debug/net8.0/task00.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task00")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4001a59af3412e458834980ea09680a6eb961129")]
 [assembly: System.Reflection.AssemblyProductAttribute("task00")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task00")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
